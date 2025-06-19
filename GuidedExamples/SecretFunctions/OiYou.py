077ae3d1b7c2259dd6c5315877fd678d6caa5949
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2025-06-19 12:22:43 +0100

    Tweak and add quote

--- GuidedExamples/SecretFunctions/OiYou.py
+++ GuidedExamples/SecretFunctions/OiYou.py
@@ -4,7 +4,8 @@
 #Don't look! One approach to good testing is to test against what a function
 # SAYS it does - not what is implemented, aka Black-Box testing
 
-#This is not the only choice, but it is something to consider!
+#This is not the only choice, but it is something to practice. So don't scroll down because
+# the bugs are obvious from the code!
 
 
 
@@ -33,13 +34,14 @@
 
 
 #OK, so you're insisting on looking anyway.....
+# ...in a locked filing cabinet stuck in a disused lavatory with a sign on the door saying “Beware of The Leopard“.
 
 from random import random
 
 def sumItems(items):
     """Sum a list of items, but with bugses"""
 
-    sum = 0.000000001
+    sum = 0.0000001
     for item in items[1:]:
         sum = sum + item
 
@@ -47,5 +49,5 @@ def sumItems(items):
 
 def sumItems2(items):
     """Sum a list of items with subtle bugs"""
-    oopsie = random() * 1e-10
+    oopsie = (random()-1.0) * 1e-10
     return sum(items) + oopsie

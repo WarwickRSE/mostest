7807bf18dce39cc68b10e42b25132353e3d94cd9
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2025-06-20 16:41:44 +0100

    Tmp - first draft code

--- GuidedExamples/scraps.py
+++ GuidedExamples/scraps.py
@@ -12,6 +12,51 @@ def myFunction():
     except Exception as e:
         print("An error occured: ", e)
 
+def host_pick_3(doors, contestant_pick):
+    # In general the host can reveal any number of doors - this is the version relying on there being 3 doors, and thus only 1 to reveal
+    # They never select the one the contestant has selected, NOR the one with the prize
+
+    #NOTE: if we want this to be properly "fair" for the statistics we need to be careful
+    # I do not promise I have been careful enough, although I think I have
+
+    n = len(doors)
+    assert n == 3, "Not implemented bigger problems yet"
+
+    prize = doors.index(True) + 1
+ 
+    #Pick random non-prize, non-contestant selected door:
+    # This eliminates either 1 or 2 doors from those the host could pick
+    # One if both conditions are the same door
+    if prize == contestant_pick:
+        choice = random_pick(n-1)
+    else:
+        choice = random_pick(n-2)
+
+    # Choice is the index into the 'available' doors - ones which are
+    # neither a prize nor the contestant's pick (remember both properties may be the same door!)
+   
+    # [g, g, c, p, g, g]
+    # [1, 2, -, -, 3, 4] # Available doors marked
+    # [g, p, g, c, g, g] #
+    # [1, -, 2, -, 3, 4]
+    # [g, p/c, g, g, g, g]
+    # [1,  -,  2, 3, 4, 5] #One more available since prize is same as contestant's pick
+
+    # Skipping over 'disallowed' doors
+    # Note - this is just fixing the index to avoid the '-' in the arrays above
+    # We are not changing the host pick here
+    # We could alternately do something like:
+    # available_doors = [1, -, -]  # could also look like: [-, 1, -] or [-, 1, 2]
+    # Choice is 1 in first two cases and either 1 or 2 in the last one
+    # choice = available_doors.index(choice) + 1
+    while(choice == prize or choice == contestant_pick):
+        choice = choice + 1
+
+    return [choice]
+
+
+
+
 
 if __name__ == "__main__":
 

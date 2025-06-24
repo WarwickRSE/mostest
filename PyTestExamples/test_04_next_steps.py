bab8e28f0d43d3cc3babd418dfd6ca62cda1b907
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2025-06-24 16:15:44 +0100

    Another note

--- PyTestExamples/test_04_next_steps.py
+++ PyTestExamples/test_04_next_steps.py
@@ -7,6 +7,11 @@
 #If not, we've provided a couple of TestableCode files you can use. 
 # Pick one, have a read, run the code, and then start writing a test suite. 
 
+#IMPORTANT: it's OK, in fact it's great!, if your tests reveal potential 'oversights' in
+# the code, such as assuming things which are not verified. As we've said, it's not _always_
+# the case that we should add every check we might think of. See DefensiveCoding for some
+# notes on why not. 
+
 #We don't have model solutions for this section - we have some hints, and we
 # can give you feedback - just ask!
 
